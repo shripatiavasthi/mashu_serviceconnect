35e0288948818aed7321bf50a5f5af6a8405a8ff
verb-shripati <73355006+shripatiavasthi@users.noreply.github.com>
2025-12-16 22:57:28 +0530

    added

--- shared/DynamicText/rich.jsx
+++ shared/DynamicText/rich.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
-import { useWindowDimensions, Linking } from 'react-native';
+import { Linking, StyleSheet } from 'react-native';
 import { useTheme } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
-import RenderHtml, { defaultSystemFonts } from 'react-native-render-html';
+import HTMLView from 'react-native-htmlview';
 
 const HexColorRegex = /#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})/g
 
 export default function RichText({ text, style }) {
   const navigation = useNavigation();
-  const { width } = useWindowDimensions();
   const { fontFamily } = useTheme().theme;
-  const systemFonts = [
-    fontFamily.bold,
-    fontFamily.regular,
-    ...defaultSystemFonts,
-  ];
-  const tagsStyles = { p: { paddingTop: 0, marginTop: 3, marginBottom: 2, color: style?.color || '#000000' }, body: { color: style?.color }, a: { color: '#036CB6', fontWeight: "bold" }  };
+  const htmlStyles = StyleSheet.create({
+    p: {
+      paddingTop: 0,
+      marginTop: 3,
+      marginBottom: 2,
+      color: style?.color || '#000000',
+      fontFamily: fontFamily.regular,
+    },
+    body: { color: style?.color, fontFamily: fontFamily.regular },
+    a: { color: '#036CB6', fontWeight: 'bold', fontFamily: fontFamily.regular },
+  });
   if (text == null || text == '') return null;
   let formattedText = text.replace(HexColorRegex, "'$1'")  //add '' in all hex values
   
   return (
-    <RenderHtml
-      tagsStyles={tagsStyles}
-      contentWidth={width}
-      source={{ html: formattedText }}
-      systemFonts={systemFonts}
-      renderersProps={{ a: { onPress: (evt, href) => { Linking.openURL(href) } } }}
+    <HTMLView
+      value={formattedText}
+      stylesheet={htmlStyles}
+      onLinkPress={(url) => Linking.openURL(url)}
     />
   );
 }
